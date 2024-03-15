90bdaae0b8c3d903670cf9c851dfef63c1d90611
Mihai-Alexandru Vistran <93713092+Xernaxe@users.noreply.github.com>
2024-03-15 21:34:21 +0200

    package.json fix

--- src/_data-entry/matches.tsx
+++ src/_data-entry/matches.tsx
@@ -1,13 +1,15 @@
 import { IMatchData } from '../_types/IMatchData';
 export type MatchType = {};
 
-const URL = 'http://livescore-api.com/api-client/matches/live.json?';
-export const getMatches = async (searchParams: URLSearchParams = new URLSearchParams()) => {
+const API_KEY = import.meta.env.VITE_LIVE_SCORE_API_KEY;
+const API_SECRET = import.meta.env.VITE_LIVE_SCORE_API_SECRET;
+const API_URL = 'http://livescore-api.com/api-client/matches/live.json?';
+export const getMatches = async (
+	searchParams: URLSearchParams = new URLSearchParams()
+) => {
 	try {
 		const response = await fetch(
-			`${URL}key=${import.meta.env.VITE_LIVE_SCORE_API_KEY}&secret=${
-				import.meta.env.VITE_LIVE_SCORE_API_SECRET
-			}&${searchParams}`
+			`${API_URL}key=${API_KEY}&secret=${API_SECRET}&${searchParams}`
 		);
 		const data = await response.json();
 
